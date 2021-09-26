e616d88f238b1d2625deed228b4129d869597878
theandrewlevin <91433038+theandrewlevin@users.noreply.github.com>
2021-09-26 22:13:13 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+sudo apt install screen tmux tree git wget curl unzip htop httpie ripgrep ncdu
+
+screen
+
+sudo apt install unattended-upgrades
+sudo dpkg-reconfigure --priority=low unattended-upgrades
+
+sudo timedatectl set-timezone Europe/Moscow
+sudo apt-get install ntp -y
+
+sudo apt update && sudo apt upgrade
